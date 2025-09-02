721eaad996f173f59db9bcfacdf96a9f580b0652
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-09-02 20:48:56 +0300

    Initialize database schema for book store with tables for Book, Author, Customer, Order, and Order_Details.

---
+++ alx_book_store.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Book(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id FOREIGN KEY REFERENCES Author(author_id),
+    price DOUBLE NOT NULL,
+    publication_date DATE NOT NULL
+);
+
+CREATE TABLE Author(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE Customer(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT NOT NULL
+)
+
+CREATE TABLE Order(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT FOREIGN KEY REFERENCES Customer(customer_id),
+    order_date DATE NOT NULL
+)
+
+CREATE TABLE Order_Details(
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT FOREIGN KEY REFERENCES Order(order_id),
+    book_id INT FOREIGN KEY REFERENCES Book(book_id),
+    quantity DOUBLE NOT NULL
+)
