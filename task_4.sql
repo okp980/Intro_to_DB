7ec76b9424113b006d6fbe37c583e992a66c5d10
Okpunor Emmanuel <okpunorrex@gmail.com>
2025-09-03 10:16:46 +0300

    Update SQL query in task_4.sql to include COLUMN_TYPE and correct table name to 'Books'

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,6 @@
 SELECT 
     COLUMN_NAME,
+    COLUMN_TYPE,
     DATA_TYPE,
     IS_NULLABLE,
     COLUMN_KEY,
@@ -9,6 +10,6 @@ FROM
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books'
+    AND TABLE_NAME = 'Books'
 ORDER BY 
     ORDINAL_POSITION;
